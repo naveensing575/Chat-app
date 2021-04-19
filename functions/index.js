76544bcb6518ccd1c9d243db2c700da7bac76bd1
navi107 <naveensing575@gmail.com>
2021-04-19 14:51:57 +0530

    tested

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,9 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const serviceAccount = require('./service-account.json');
+const functions = require("firebase-functions");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://chat-web-app-4ee4c.firebaseio.com',
-});
-
-const { sendFcm } = require('./src/fcm');
-
-exports.sendFcm = sendFcm;
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
